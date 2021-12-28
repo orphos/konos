3d123824837240dd1df5a8392c2aa859e1ee0305
Takezoe, Toamoki <tomoaki3478@balux.org>
2021-12-28 14:04:52 +0900

    Do not run print-git-dirty-mark on MSYS2

--- .config/zsh/conf.d/20-prompt.zsh
+++ .config/zsh/conf.d/20-prompt.zsh
@@ -1,12 +1,13 @@
 setopt prompt_subst
 
 _git_prompt() {
+
   if test -d .git || git rev-parse --git-dir 1>/dev/null 2>/dev/null; then
     echo ''
     echo -n '<%F{002}'
     echo -n "$(git branch 2>/dev/null | sed -n '/\* /s///p')"
     echo -n '%f>'
-    if test -n "$(git rev-parse --show-toplevel)" ; then
+    if [[ $(uname) != MSYS_* && -n $(git rev-parse --show-toplevel) ]] ; then
       echo -n "%F{002}$(~/.config/zsh/util/print-git-dirty-mark)%f"
     fi
   fi
